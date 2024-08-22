f5d0bc444e8e16f05829f7893802a17dbbd2b5e4
gygabytedev <podvirnyidmytro@outlook.com>
2024-08-22 21:13:57 +0300

    update design

--- src/components/Slider/Slider.component.tsx
+++ src/components/Slider/Slider.component.tsx
@@ -37,23 +37,61 @@ const Slider: FC<SliderProps> = ({ value, setValue }) => {
   };
 
   return (
-    <VerticalSlider
-      value={value}
-      onChange={(value) => setValue(value)}
-      height={170}
-      width={14}
-      step={1}
-      min={0}
-      max={100}
-      borderRadius={10}
-      maximumTrackTintColor="transparent"
-      minimumTrackTintColor="transparent"
-      renderIndicatorHeight={30}
-      showIndicator
-      renderIndicator={() => <Indicator />}
-      containerStyle={sliderContainer}
-      sliderStyle={slider}
-    />
+    <View>
+      <View
+        style={{
+          height: 1,
+          width: 8,
+          borderTopWidth: 0.8,
+          borderColor: "#30324E",
+          opacity: 0.2,
+          position: "absolute",
+          top: 120,
+          left: 3,
+        }}
+      />
+      <View
+        style={{
+          height: 1,
+          width: 8,
+          borderTopWidth: 0.8,
+          borderColor: "#30324E",
+          opacity: 0.2,
+          position: "absolute",
+          top: 80,
+          left: 3,
+        }}
+      />
+      <View
+        style={{
+          height: 1,
+          width: 8,
+          borderTopWidth: 0.8,
+          borderColor: "#30324E",
+          opacity: 0.2,
+          position: "absolute",
+          top: 40,
+          left: 3,
+        }}
+      />
+      <VerticalSlider
+        value={value}
+        onChange={(value) => setValue(value)}
+        height={170}
+        width={14}
+        step={1}
+        min={0}
+        max={100}
+        borderRadius={10}
+        maximumTrackTintColor="transparent"
+        minimumTrackTintColor="transparent"
+        renderIndicatorHeight={30}
+        showIndicator
+        renderIndicator={() => <Indicator />}
+        containerStyle={sliderContainer}
+        sliderStyle={slider}
+      />
+    </View>
   );
 };
 
